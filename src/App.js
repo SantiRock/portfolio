81c44bbfc996b400890e06ebe0f0dfc6533fc270
Santiago Quintero <santiago.qp86@gmail.com>
2023-04-19 20:17:31 -0500

    lang 1

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
 import { useState } from 'react';
 import './App.css';
 import Head from './js/head.js';
-import LangApp from './js/body'
+import LangApp from './js/body';
+import EsApp from './js/bodyes';
+
 import { acerca, about } from './js/about';
 import { projects, proyectos } from './js/projects';
 
 
 const App = () => {
   const [showHead, setShowHead] = useState( about )
-  const [showBody, setShowBody] = useState( projects )
+  const [currentComponent, setCurrentComponent] = useState( <LangApp arr={projects}/> )
 
 
   const handlerEn = (event) => {
     event.preventDefault();
     setShowHead(about)
-    setShowBody(projects)
+    setCurrentComponent(<LangApp arr={projects}/>)
   }
 
   const handlerEs = (event) => {
     event.preventDefault();
     setShowHead(acerca)
-    setShowBody(proyectos)
+    setCurrentComponent(<EsApp arr={proyectos}/>)
   }
 
   return (
     <>
       <Head arr={showHead} handlerEn={handlerEn} handlerEs={handlerEs}/>
-      <LangApp arr={showBody} />
+      {currentComponent}
     </>
   );
 }
--- src/js/bodyes.js
+++ src/js/bodyes.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { proyectos } from './projectsbeta';
 
 const Btn = ( {text, handler} ) => <button className='btn' onClick={handler}>{text}</button>
 
@@ -9,8 +8,8 @@ const Project = ( {link, name, tecnologies, description, role, year } ) => {
       <a href={link} target="_blank">
         <h3>{name}</h3>
         <p className='techs'>- {tecnologies.join(', ')}</p>
-        <p><b>Role:</b> {role}</p>
-        <p><b>Year:</b> {year}</p>
+        <p><b>Rol:</b> {role}</p>
+        <p><b>Año:</b> {year}</p>
         <p>{description}</p>
       </a>
     </div>
@@ -22,38 +21,39 @@ showAll.map(({link, name, tecnologies, description, role, id, year}) =>
 <Project key={id} link={link} name={name} tecnologies={tecnologies} description={description} role={role} year={year}/>)
 
 
-const EsApp = ({}) => {
-  const [showAll, setShowAll] = useState(proyectos)
+const EsApp = ({arr}) => {
+  const proj = arr[0].projects
+  const [showAll, setShowAll] = useState(proj)
 
   const jsBtn = (event) => {
     event.preventDefault();
-    setShowAll(proyectos.filter(project => project.tags === 'javascript'))
+    setShowAll(proj.filter(project => project.tags === 'javascript'))
   }
 
   const reactBtn = (event) => {
     event.preventDefault();
-    setShowAll(proyectos.filter(project => project.tags === 'react'))
+    setShowAll(proj.filter(project => project.tags === 'react'))
   }
 
   const htmlBtn = (event) => {
     event.preventDefault();
-    setShowAll(proyectos.filter(project => project.tags === 'html'))
+    setShowAll(proj.filter(project => project.tags === 'html'))
   }
 
   const allBtn = (event) => {
     event.preventDefault();
-    setShowAll(proyectos)
+    setShowAll(proj)
   }
 
 
   return (
     <section className='prss'>
-      <h2>Proyectos</h2>
+      <h2>{arr[0].title}</h2>
       <div className='btns'>
         <Btn text='React' handler={reactBtn}/>
         <Btn text='JavaScript' handler={jsBtn}/>
         <Btn text='HTML & CSS' handler={htmlBtn}/>
-        <Btn text='Todos' handler={allBtn}/>
+        <Btn text={arr[0].all} handler={allBtn}/>
       </div>
       <div className='projects'>
         <Projects showAll={showAll}/>
