6807da0178f72f425c73b20e1ee882b3dbb77f61
SantiRock <sqp.dev@gmail.com>
2026-05-13 16:20:24 -0500

    multi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ export function Home () {
         <div class={styles.main}>
             <div class={styles.sc}>
                 <div class={styles.statment}>
-                    <p class={styles.p1}>Profissional colombiano com formação em cinema e mestrado em artes digitais, com um perfil multifacetado voltado para o setor audiovisual, as artes do espetáculo e os eventos ao vivo. Minha abordagem se baseia em entender a imagem, a luz e o som como um sistema integrado, com capacidade de me adaptar a diferentes contextos culturais e técnicos. Minhas principais áreas de atuação são a produção e coordenação e o vídeo em tempo real e video-mapping, complementadas por explorações em iluminação, desenvolvimento web, som e artes visuais.</p>
+                    <p class={styles.p1}>Profissional colombiano com formação em cinema e mestrado em artes digitais, com um perfil voltado para o setor audiovisual, as artes do espetáculo e os eventos ao vivo. Minha abordagem se baseia em entender a imagem, a luz e o som como um sistema integrado, com capacidade de me adaptar a diferentes contextos culturais e técnicos. Minhas principais áreas de atuação são a produção e coordenação e o vídeo em tempo real e video-mapping, complementadas por explorações em iluminação, desenvolvimento web, som e artes visuais.</p>
                 </div>
                 <div class={styles.statment}>
                     <p class={styles.p1}>Minha trajetória combina experiência técnica, sensibilidade artística e capacidade organizacional, integrando essas três dimensões de forma natural em cada projeto.</p>
